4d27df694a069c6532541299f3479f93d126c09a
enicorn-evan <64674110+enicorn-evan@users.noreply.github.com>
2020-06-01 22:31:00 +0800

    Add: Activities Done Notification

---
+++ activity_done_notification/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Activity Done Notification',
+    'version': '13.0',
+    'category': 'Mail Notification',
+    'license': 'OPL-1',
+    'summary': 'Notify assigner when assignee marks activity done.',
+
+    'author': 'Enicorn Limited',
+    'maintainer': 'Enicorn Limited',
+
+    'depends': ['mail'],
+    'data': [
+        'data/email_template.xml',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'price': 30.00,
+    'currency': 'USD',
+}
---
+++ activity_done_notification/models/mail_activity.py
@@ -0,0 +1,27 @@
+from odoo import models, _
+
+
+class MailActivityExt(models.Model):
+    _inherit = 'mail.activity'
+
+    def _action_done(self, feedback=False, attachment_ids=None):
+        body_template = self.env.ref('activity_done_notification.message_activity_done')
+        for activity in self:
+            model_description = self.env['ir.model']._get(activity.res_model).display_name
+            body = body_template.render(
+                dict(activity=activity, model_description=model_description, assignee=self.env.user.name),
+                engine='ir.qweb',
+                minimal_qcontext=True
+            )
+            record = self.env[activity.res_model].browse(activity.res_id)
+            if activity.user_id:
+                record.message_notify(
+                    partner_ids=activity.create_uid.partner_id.ids,
+                    body=body,
+                    subject=_('%s is done with the assigned activity %s by you.') % (self.env.user.name, activity.summary or activity.activity_type_id.name),
+                    record_name=activity.res_name,
+                    model_description=model_description,
+                    email_layout_xmlid='mail.mail_notification_light',
+                )
+        return super(MailActivityExt, self)._action_done()
+
